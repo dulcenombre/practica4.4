409ad99b6597c8f39d919a12e3f185958ba26102
usuario <usuario@EQUIPO05-PC>
2017-03-31 13:59:51 +0200

    Modificación Alejandro

--- practica4-4/src/practica4/pkg4/Practica44.java
+++ practica4-4/src/practica4/pkg4/Practica44.java
@@ -29,7 +29,7 @@ public class Practica44 {
             System.out.println("3");
 
             System.out.println("4");
-            System.out.println("5");
+            System.out.println("5: Alejandro");
             System.out.println("6");
             System.out.println("7");
             System.out.println("8");
